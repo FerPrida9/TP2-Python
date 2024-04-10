e5e00deb0e5e012fce5258a5cae0278b5306f451
Fermin Manuel Prida <ferminprida97@gmail.com>
2024-04-10 12:17:50 -0300

    Inciso 2

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,5 +1,24 @@
 def estructura (names, goals, goals_avoided, assists):
+    """ esta funcion recibe 4 listas y retorna un diccionario que tiene
+    como llave los nombres de los jugadores y como valor una tupla de 
+    3 elementos (goles, goles evitados, asistencias)"""
     players = {}
     for name, g, ga, a in zip(names, goals, goals_avoided, assists):
         players[name] = (g,ga,a)
     return players
+
+def goal_scorer (players):
+    """ esta funcion recibe el diccionario de jugadores y retorna una tupla
+    con el nombre y los goles del goleador/goleadora"""
+    max_goals = -1
+    for player, values in players.items():
+        if values[0] > max_goals:
+            max_goals = values[0]
+            max_player = player
+    max = (max_player, max_goals)
+    return max
+    
+
+         
+
+
